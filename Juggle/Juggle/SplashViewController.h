0e6eb2c0e4791db9b48249dc0b066dcc26f07a73
Dave Hendrix <dave@rogueminds.net>
2011-09-07 15:55:15 -0700

    Fixed UI glitches, added display of local and remote data

---
+++ Juggle/Juggle/SplashViewController.h
@@ -0,0 +1,13 @@
+//
+//  SplashViewController.h
+//  Juggle
+//
+//  Created by Dave Hendrix on 9/6/11.
+//  Copyright 2011 RogueMinds.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SplashViewController : UIViewController
+
+@end
